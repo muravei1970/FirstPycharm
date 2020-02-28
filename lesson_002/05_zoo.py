ec756ea07a876383efd81c6d70bdbb08733adb3c
Andrey <muravei1970@yandex.ru>
2020-02-28 18:54:55 +0300

    Дом_задание_02_05

---
+++ lesson_002/05_zoo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# есть список животных в зоопарке
+
+zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
+
+# посадите медведя (bear) между львом и кенгуру
+#  и выведите список на консоль
+
+zoo.insert(1, "bear")
+print(zoo)
+
+# добавьте птиц из списка birds в последние клетки зоопарка
+birds = ['rooster', 'ostrich', 'lark']
+#  и выведите список на консоль
+
+zoo.append(birds[0])
+zoo.append(birds[1])
+zoo.append(birds[2])
+print(zoo)
+
+# уберите слона
+#  и выведите список на консоль
+
+del zoo[3]
+print(zoo)
+
+# выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
+# Номера при выводе должны быть понятны простому человеку, не программисту.
+
+print(zoo.index('lion') + 1, zoo.index('lark') + 1)
+
