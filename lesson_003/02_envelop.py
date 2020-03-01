6f6846b50c5008385d418307c770cb0650217cc3
Andrey <muravei1970@yandex.ru>
2020-03-01 20:13:25 +0300

    Дом_задание_03_02

---
+++ lesson_003/02_envelop.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+# (if/elif/else)
+
+# Заданы размеры envelop_x, envelop_y - размеры конверта и paper_x, paper_y листа бумаги
+#
+# Определить, поместится ли бумага в конверте (стороны листа параллельны сторонам конверта)
+#
+# Результат проверки вывести на консоль (ДА/НЕТ)
+# Использовать только операторы if/elif/else, можно вложенные
+
+envelop_x, envelop_y = 10, 7
+paper_x, paper_y = 8, 9
+# проверить для
+# paper_x, paper_y = 9, 8
+# paper_x, paper_y = 6, 8
+# paper_x, paper_y = 8, 6
+# paper_x, paper_y = 3, 4
+# paper_x, paper_y = 11, 9
+# paper_x, paper_y = 9, 11
+# (просто раскоментировать нужную строку и проверить свой код)
+
+# Определить максимальную и минимальную стороны конверта
+if envelop_x >= envelop_y:
+    max_size_envelop = envelop_x
+    min_size_envelop = envelop_y
+else:
+    max_size_envelop = envelop_y
+    min_size_envelop = envelop_x
+
+# Определить максимальную и минимальную стороны листа
+if paper_x >= paper_y:
+    max_size_paper = paper_x
+    min_size_paper = paper_y
+else:
+    max_size_paper = paper_y
+    min_size_paper = paper_x
+
+# Если максимальная сторона листа меньше либо равна максимальной стороне конверта и
+# минимальная сторона листа меньше либо равна минимальной стороне конверта - ДА - лист войдет в конверт
+if max_size_paper <= max_size_envelop and min_size_paper <= min_size_envelop:
+    print("ДА")
+else:
+    print("НЕТ")
+
+# Усложненное задание, решать по желанию.
+# Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
+# могут быть в диапазоне от 1 до 1000)
+#
+# Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
+
+hole_x, hole_y = 8, 9
+brick_x, brick_y, brick_z = 11, 10, 2
+# brick_x, brick_y, brick_z = 11, 2, 10
+# brick_x, brick_y, brick_z = 10, 11, 2
+# brick_x, brick_y, brick_z = 10, 2, 11
+# brick_x, brick_y, brick_z = 2, 10, 11
+# brick_x, brick_y, brick_z = 2, 11, 10
+# brick_x, brick_y, brick_z = 3, 5, 6
+# brick_x, brick_y, brick_z = 3, 6, 5
+# brick_x, brick_y, brick_z = 6, 3, 5
+# brick_x, brick_y, brick_z = 6, 5, 3
+# brick_x, brick_y, brick_z = 5, 6, 3
+# brick_x, brick_y, brick_z = 5, 3, 6
+# brick_x, brick_y, brick_z = 11, 3, 6
+# brick_x, brick_y, brick_z = 11, 6, 3
+# brick_x, brick_y, brick_z = 6, 11, 3
+# brick_x, brick_y, brick_z = 6, 3, 11
+# brick_x, brick_y, brick_z = 3, 6, 11
+# brick_x, brick_y, brick_z = 3, 11, 6
+# (просто раскоментировать нужную строку и проверить свой код)
+
+# Определить максимальную и минимальную стороны отверстия
+if hole_x >= hole_y:
+    max_size_hole = hole_x
+    min_size_hole = hole_y
+else:
+    max_size_hole = hole_y
+    min_size_hole = hole_x
+
+# Определить максимальную, среднюю и минимальную стороны кирпича
+if brick_x >= brick_y >= brick_z:
+    max_size_brick = brick_x
+    middle_size_brick = brick_y
+    min_size_brick = brick_z
+elif brick_x >= brick_z >= brick_y:
+    max_size_brick = brick_x
+    middle_size_brick = brick_z
+    min_size_brick = brick_y
+elif brick_y >= brick_x >= brick_z:
+    max_size_brick = brick_y
+    middle_size_brick = brick_x
+    min_size_brick = brick_z
+elif brick_y >= brick_z >= brick_x:
+    max_size_brick = brick_y
+    middle_size_brick = brick_z
+    min_size_brick = brick_x
+elif brick_z >= brick_x >= brick_y:
+    max_size_brick = brick_z
+    middle_size_brick = brick_x
+    min_size_brick = brick_y
+elif brick_z >= brick_y >= brick_x:
+    max_size_brick = brick_z
+    middle_size_brick = brick_y
+    min_size_brick = brick_x
+
+# Если средняя сторона кирпича меньше либо равна максимальной стороне отверстия и
+# минимальная сторона кирпича меньше либо равна минимальной стороне отверстия - ДА - кирпич пройдет через отверстие
+if middle_size_brick <= max_size_hole and min_size_brick <= min_size_hole:
+    print("ДА")
+else:
+    print("НЕТ")
